04c96fb5254c802143b53c4f0f97aaeb15932af9
Elzaido <91469219+Elzaido@users.noreply.github.com>
2023-08-22 18:42:57 +0300

    Master

--- lib/shared/ReadMe.dart
+++ lib/shared/ReadMe.dart
@@ -1,1 +1,4 @@
-// State management solution used for this project is 'Bloc' ... I'am currently working on learn how to handle other State management solutions. 
+// ignore_for_file: file_names
+
+
+////// State management solution used for this project is 'Bloc' ... I'am currently working on learn how to handle other State management solutions. 
